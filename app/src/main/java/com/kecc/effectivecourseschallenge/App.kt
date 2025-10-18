5a8b86f9da364e500376ebfbeecd4eb134b427ad
username <somemail@example.com>
2025-10-18 12:43:04 +0500

    inital commit

---
+++ app/src/main/java/com/kecc/effectivecourseschallenge/App.kt
@@ -0,0 +1,11 @@
+package com.kecc.effectivecourseschallenge
+
+import android.app.Activity
+import android.os.Bundle
+
+class App : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_main)
+    }
+}
