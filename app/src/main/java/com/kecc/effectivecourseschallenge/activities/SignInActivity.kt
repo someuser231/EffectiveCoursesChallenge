7290e8160a6fa943303a9da83bcf9e06737ac1f7
username <somemail@example.com>
2025-10-19 22:58:43 +0500

    sign in screen main design

--- app/src/main/java/com/kecc/effectivecourseschallenge/activities/SignInActivity.kt
+++ app/src/main/java/com/kecc/effectivecourseschallenge/activities/SignInActivity.kt
@@ -7,12 +7,10 @@ import androidx.appcompat.app.AppCompatActivity
 import com.kecc.effectivecourseschallenge.databinding.ActivitySignInBinding
 import com.kecc.effectivecourseschallenge.fragments.SignInFrg
 import com.kecc.effectivecourseschallenge.view_models.SignInViewModel
-import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SignInActivity : AppCompatActivity() {
     val signInViewModel: SignInViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         signInViewModel.appBinding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(signInViewModel.appBinding.root)
